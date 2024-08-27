2478bc085a26e407346b9d46e658eb2fda42e66e
DarDuong5 <140448684+DarDuong5@users.noreply.github.com>
2024-08-27 19:46:59 -0400

    Update main.py
    added insert, get, update, and delete functions

--- keyval-store/main.py
+++ keyval-store/main.py
@@ -1,7 +1,24 @@
 class KeyValueStore:
     def __init__(self):
         self.store = {}
+
+    def insert(self, key, value):
+        self.store[key] = value
     
-if __name__ == '__main__':
-    kvStore = KeyValueStore()
-    print("Key value instance created", kvStore)
+    def get(self, key):
+        try:
+            return self.store.get(key)
+        except Exception as e:
+            print(f"Key {e} does not exist")
+
+    def update(self, key, value):
+        try:
+            self.store[key] = value
+        except Exception as e:
+            print(f"Cannot find the key to update: {e}")
+    
+    def delete(self, key):
+        try:
+            del self.store[key]
+        except Exception as e:
+            print(f"Cannot find find the key to delete: {e}")
