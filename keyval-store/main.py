07a89b2bd7504d421ecbfc28321c90817d56a60b
DarDuong5 <140448684+DarDuong5@users.noreply.github.com>
2024-08-27 18:40:57 -0400

    Add files via upload

---
+++ keyval-store/main.py
@@ -0,0 +1,7 @@
+class KeyValueStore:
+    def __init__(self):
+        self.store = {}
+    
+if __name__ == '__main__':
+    kvStore = KeyValueStore()
+    print("Key value instance created", kvStore)
