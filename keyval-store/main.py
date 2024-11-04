e1ad45a3a1a3170a1de803b38ad003cb29f38223
DarDuong5 <140448684+DarDuong5@users.noreply.github.com>
2024-11-03 19:46:37 -0500

    Update main.py

--- keyval-store/main.py
+++ keyval-store/main.py
@@ -1,52 +1,51 @@
-import os
-import json
+import click
+from kvstore import KeyValueStore
 
-class KeyValueStore:
-    def __init__(self, fileName='data.json'):
-        self.fileName = fileName
-        self.store = self.readFromFile()
+kv_store = KeyValueStore(fileName='data.json')
 
-    def readFromFile(self):
-        if os.path.exists(self.fileName):
-            try:
-                with open(self.fileName, 'r') as file:
-                    return json.load(file)
-            except json.JSONDecodeError:
-                print(f"Warning: Could not decode the JSON file {self.fileName}. Initializing empty store.")
-                return {}
-        else:
-            return {}
+@click.group()
+def cli():
+    """ A CLI for Key-Value Store """
+    pass
+
+@cli.command()
+@click.argument("key")
+@click.argument("value")
+def insert(key, value):
+    """ Inserts given key and value into key-value store """
+    kv_store.insert(key, value)
+    click.echo(f"Inserted {key}: {value}")
+
+@cli.command()
+@click.argument("key")
+def read(key):
+    """ Reads the value of the key """
+    value = kv_store.get(key)
+    if value is not None:
+        click.echo(f"{key}: {value}")
+    else:
+        click.echo("This key does not exist")
+
+@cli.command()
+@click.argument("key")
+@click.argument("value")
+def update(key, value):
+    """ Updates the value of the key """
+    if kv_store.get(key) is not None:
+        kv_store.update(key, value)
+        click.echo(f"Updated! {key}: {value}")
+    else:
+        click.echo("This key does not exist")
     
-    def writeToFile(self):
-        with open(self.fileName, 'w') as file:
-            json.dump(self.store, file, indent=4)
-            
-    def insert(self, key, value):
-        self.store[key] = value
-        self.writeToFile()
-    
-    def get(self, key):
-        try:
-            return self.store.get(key, None)
-        except Exception as e:
-            print(f"Key {e} does not exist")
+@cli.command()
+@click.argument("key")
+def delete(key):
+    """ Deletes the key and its value """
+    if kv_store.get(key) is not None:
+        kv_store.delete(key)
+        click.echo(f"Successfully deleted {key}")
+    else:
+        click.echo("This key does not exist")
 
-    def update(self, key, value):
-        try:
-            self.store[key] = value
-            self.writeToFile()
-        except Exception as e:
-            print(f"Cannot find the key to update: {e}")
-    
-    def delete(self, key):
-        try:
-            del self.store[key]
-            self.writeToFile()
-        except Exception as e:
-            print(f"Cannot find find the key to delete: {e}")
-
-dictionary1 = KeyValueStore()
-
-dictionary1.insert("hello", "world")
-dictionary1.insert("I love", "computer science")
-dictionary1.update("hello", "everyone")
+if __name__ == '__main__':
+    cli()
